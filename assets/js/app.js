b2c8a8e4770dc709a8318980916456cbc205c8ef
Gianni <gianniricciardi@3779@gmail.com>
2023-04-12 20:24:02 -0300

    Agregada funcionalidad para menu hamburguesa

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// Funcion que accionan el boton del menu responsive
+(function () {
+    let boton = document.getElementById("boton");
+    let nav = document.getElementById("menu");
+    let con = true;
+    function menu() {
+      if (con) {
+        nav.style.right = "0";
+        con = false;
+      } else {
+        nav.style.right = "-200%";
+        con = true;
+      }
+    }
+  
+    boton.addEventListener("click", menu);
+  
+    $(".item").click(function () {
+      let target = $(this).parent().children(".slide");
+      $(target).slideToggle();
+    });
+  })();
+  
+  
